05bf123a0ed62f3f237fe0111b28c05dccb5bc69
guilhermegomees <guicollado1@gmail.com>
2022-07-13 21:38:36 -0300

    Implementing simple search

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Services.Exceptions;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc.Services
 {
